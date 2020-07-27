2c29e283e3060912fbbb4ad69e826ad1ef765711
Kimeg <kimeg777@naver.com>
2020-07-27 11:37:04 +0900

    Comments added

--- client.py
+++ client.py
@@ -11,16 +11,6 @@ DISCONNECT= "quit"
 SERVER = socket.gethostbyname(socket.gethostname());
 ADDR = (SERVER, PORT)
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-client.connect(ADDR)
-
-connection = True
-received = False
-sent = False
-
-received_msg = None
-sent_msg = None
-
 def mapper(v):
     if v in PAPERS:
         return 'P'
@@ -30,79 +20,112 @@ def mapper(v):
         return 'R' 
     return None
 
-def listen(client):
-    global sent, received, received_msg, sent_msg
-    from_server = client.recv(BYTE_SIZE).decode(FORMAT)
-    if not from_server=='' and not 'USER' in from_server:
-        if received:
-            return
+class Client:
+    def __init__(self, client):
+        self.client = client
+        self.connection = True
+        self.received = False
+        self.sent = False
 
-        received = True
+        self.received_msg = None
+        self.sent_msg = None
 
-        if sent:        
-            mine = mapper(sent_msg)
-            opp = mapper(from_server)
-            print(f"\nYOU : {MAPPED[mine]} VS OPPONENT : {MAPPED[opp]}\n")
-            print(f"\n{CASES[mine+opp]}\n")
-                
-            received = False
-            sent = False
-        else:
-            print("\nOPPONENT HAS SENT ROCK/PAPER/SCISSORS.\n\nYOUR TURN.\n")
-            received_msg = from_server
-    return
+    def listen(self):
+        # message from the server
+        from_server = self.client.recv(BYTE_SIZE).decode(FORMAT)
 
-def send():
-    global sent, received, received_msg, sent_msg
-    while True:
-        if sent:
-            continue
+        # Checks if message received from the server is the one from another client (opponent).
+        if not from_server=='' and not 'USER' in from_server:
+            if self.received:
+                return
 
-        msg = input('\nType one of the following:\nRock/Paper/Scissors\n')
-        message = msg.encode(FORMAT)
-        msg_length = len(message)
+            # Received a message from the opponent.
+            self.received = True
 
-        send_length = str(msg_length).encode(FORMAT)
-        send_length += b' '*(BYTE_SIZE-len(send_length))
+            if self.sent:        
+                # if you already have sent your message to the opponent,
+                # reveals messages from both sides.
+                mine = mapper(self.sent_msg)
+                opp = mapper(from_server)
 
-        try:
-            if received:
-                mine = mapper(message.decode(FORMAT))
-                opp = mapper(received_msg)
-                try:
-                    print(f"\nYOU : {MAPPED[mine]} VS OPPONENT : {MAPPED[opp]}\n")
-                    print(f"\n{CASES[mine+opp]}\n")
-                    client.send(send_length)
-                    client.send(message)
-                    sent = False
-                    received = False
-                except:
-                    print(f"\nInvalid input. Type one of the following:\nRock/Paper/Scissors\n")
-
-
+                print(f"\nYOU : {MAPPED[mine]} VS OPPONENT : {MAPPED[opp]}\n")
+                print(f"\n{CASES[mine+opp]}\n")
+                    
+                self.received = False
+                self.sent = False
             else:
-                sent_msg = message.decode(FORMAT)
-                mine = mapper(sent_msg)
-                try:
-                    print(f"\nYOU SENT {MAPPED[mine]}\n\nWAITING FOR OPPONENT's TURN.\n")
-                    sent = True
-                    client.send(send_length)
-                    client.send(message)
-                except:
-                    print(f"\nInvalid input. Type one of the following:\nRock/Paper/Scissors\n")
-                
-        except socket.error as e:
-            connection = False
-            print(e)
-            break
-    return
+                # if you have not yet sent your message to the opponent,
+                # you are only notified of successful message retrival.
+                # (this makes sense since you have to send yours before revealing opponent's.)
+                print("\nOPPONENT HAS SENT ROCK/PAPER/SCISSORS.\n\nYOUR TURN.\n")
+                self.received_msg = from_server
+        return
+
+    def send(self):
+        while True:
+            # If you have already sent a message, then you cannot send another one until 
+            # you received a message from the opponent.
+            if self.sent:
+                continue
+
+            # Type one of the three choices to be sent to the opponent.
+            msg = input('\nType one of the following:\nRock/Paper/Scissors\n')
+            message = msg.encode(FORMAT)
+            msg_length = len(message)
+
+            send_length = str(msg_length).encode(FORMAT)
+            send_length += b' '*(BYTE_SIZE-len(send_length))
+
+            try:
+                if self.received:
+                    # If you have received a message from the opponent,
+                    # reveals messages from both sides.
+                    mine = mapper(message.decode(FORMAT))
+                    opp = mapper(self.received_msg)
+                    try:
+                        print(f"\nYOU : {MAPPED[mine]} VS OPPONENT : {MAPPED[opp]}\n")
+                        print(f"\n{CASES[mine+opp]}\n")
+                        self.client.send(send_length)
+                        self.client.send(message)
+                        self.sent = False
+                        self.received = False
+                    except:
+                        print(f"\nInvalid input. Type one of the following:\nRock/Paper/Scissors\n")
+
+
+                else:
+                    # If you have not yet received a message from the opponent,
+                    # you are informed that you have sent a message.
+                    self.sent_msg = message.decode(FORMAT)
+                    mine = mapper(self.sent_msg)
+                    try:
+                        print(f"\nYOU SENT {MAPPED[mine]}\n\nWAITING FOR OPPONENT's TURN.\n")
+                        self.sent = True
+                        self.client.send(send_length)
+                        self.client.send(message)
+                    except:
+                        print(f"\nInvalid input. Type one of the following:\nRock/Paper/Scissors\n")
+                    
+            except socket.error as e:
+                self.connection = False
+                print(e)
+                break
+        return
 
 def main():
-    thread = threading.Thread(target=send)
+    client = Client(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    client.client.connect(ADDR)
+
+    # Starts a new thread that ONLY deals with sending messages to the server.
+    thread = threading.Thread(target=client.send)
     thread.start()
-    while connection:
-        listen(client)
-    client.close()
+
+    # As long as the connection is valid, the client listens to incoming messages from the server.
+    # Since a single thread cannot handle both listening and sending tasks, we have assigned one thread
+    # for handling message sending and another thread for listening to incoming messages.
+    while client.connection:
+        client.listen()
+    client.client.close()
 
 if __name__=="__main__":
     main()
--- server.py
+++ server.py
@@ -10,59 +10,80 @@ ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT = "quit"
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-server.bind(ADDR)
-clients = []
-
 CONN_LIMIT = 2
-ids = defaultdict(int)
 
-def handle_client(conn, addr):
-    print(f"\n[NEW CONNECTION] USER_{ids[addr]} connected.\n")
+class Server:
+    def __init__(self, server):
+        self.server = server
+        self.clients = []
+        self.ids = defaultdict(int)
 
-    connected = True
-    while connected:
-        msg_length = conn.recv(BYTE_SIZE).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT:
-                connected = False
+    def handle_client(self, conn, addr):
+        # conn and addr objects are related to a specific client.
+        print(f"\n[NEW CONNECTION] USER_{self.ids[addr]} connected.\n")
 
-            content = "\n[USER_{0}] : {1}\n".format(str(ids[addr]), msg)
-            print(content)
-            
-            for c in clients:
-                if c == conn:
-                    continue
-                c.send(msg.encode(FORMAT))
+        connected = True
+        while connected:
+            # Receives an incoming message from the client.
+            msg_length = conn.recv(BYTE_SIZE).decode(FORMAT)
 
-    print(f"\n[DISCONNECTED] USER_{ids[addr]}\n")
-    conn.close()
-    return
+            # We have to check message length to differentiate between messages 
+            # sent by the client and an empty message. 
+            if msg_length:
+                msg_length = int(msg_length)
+                msg = conn.recv(msg_length).decode(FORMAT)
+                if msg == DISCONNECT:
+                    connected = False
+
+                content = "\n[USER_{0}] : {1}\n".format(str(self.ids[addr]), msg)
+                print(content)
+                
+                # Sends the client's message to all other clients
+                for c in self.clients:
+                    if c == conn:
+                        continue
+                    c.send(msg.encode(FORMAT))
+
+        print(f"\n[DISCONNECTED] USER_{self.ids[addr]}\n")
+        conn.close()
+        return
 
 def main():
     print("\n[SERVER START]\n")
-
-    server.listen()
+    # Generate a Server object to store connection info and clients
+    server = Server(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    # Server's socket object binds to the specified port number of localhost IP address
+    server.server.bind(ADDR)
+    # Server's socket starts to listen to incoming connections from client(s).
+    server.server.listen()
     print(f"\n[LISTENING] Server IP-Address : {SERVER}\n")
 
     count = 0
+    # Interactions between the server and potential clients happen inside the following while loop.
     while True:
         count += 1
-        conn, addr = server.accept()
-        
-        if len(clients)==CONN_LIMIT:
+        # The following line waits for a client's request for connection.
+        conn, addr = server.server.accept()
+
+        # If the number of connected clients reaches it's limit, no further requests are accepted.
+        if len(server.clients)==CONN_LIMIT:
             conn.close()
             continue
-            
-        ids[addr] = count
-        for c in clients:
-            c.send(f"\n[USER_{ids[addr]} HAS JOINED]\n".encode(FORMAT))
+        
+        # Server object keeps track of its clients by assigning them unique ID's
+        # Once a new client is connected to the server, a message is sent to each client 
+        # to inform that a new client has joined the server.
+        server.ids[addr] = count
+        for c in server.clients:
+            c.send(f"\n[USER_{server.ids[addr]} HAS JOINED]\n".encode(FORMAT))
 
-        clients.append(conn)
+        # The connection object between the server and the client is added to Server object's clients list
+        # In short, this is just to keep track of all connected clients for latter network processing.
+        server.clients.append(conn)
 
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        # From here, a new thread is assigned to deal with the connection between the server and THIS SPECIFIC client.
+        # Basically, every new client will be assigned a new thread to be handled.
+        thread = threading.Thread(target=server.handle_client, args=(conn, addr))
         thread.start()
     return
 
